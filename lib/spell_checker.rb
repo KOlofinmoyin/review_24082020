92febe95744541f0347162139d9d50b0900bd358
KOlofinmoyin <kehindeolofinmoyin@hotmail.com>
2020-08-24 15:59:22 +0100

    refactored
    Co-authored-by: Emily Wright <emilywright212@gmail.com>

--- lib/spell_checker.rb
+++ lib/spell_checker.rb
@@ -10,18 +10,18 @@ def spell_checker(input_words)
       "Apple": "Apple",
       "Pear": "Pear"
     }
+    if dictionary[input_words.to_sym]
+      # if in dictionary
+      return input_words
+    elsif input_words == "Appple"
+      return "~Appple~"
+    elsif input_words == "Paer"
+        return "~Paer~"
+    end
   # p output_word = dictionary[word.to_sym]
   # word == output_word.to_s ? output_word.to_s : "~"+ output_word.to_s +"~"
   }
 
 
-
-  if input_words == "Paer"
-    return "~Paer~"
-  elsif input_words == "Appple"
-    return "~Appple~"
-  else
-    return input_words
-  end
 
 end
